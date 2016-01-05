d70bc95a7e7ece25f6f38f4c6a691287febf4e4f
nabice <nabice@gmail.com>
2016-01-05 11:31:20 +0800

    update

--- flvcd_web/do.php
+++ flvcd_web/do.php
@@ -7,7 +7,7 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:'.$_SERVER['HTTP_USER_AGENT']));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $content = curl_exec($ch);
-$replace_str = '<form target="flvcd" action="/" method="POST"><input type="hidden" name="url" value="'.$url.'"/><input id="nabice_tv" class="button" style="width:100%;height:50px;border: 0px none;background-color:#eee;" type="submit" value="Play On TV"/></form><iframe style="width:0;height:0;display:none" name="flvcd" id="flvcd"></iframe><script>setTimeout("document.getElementById(\'nabice_tv\').style.display=\'none\';", 30000);</script>';
+$replace_str = '<form target="flvcd" action="/" method="POST"><input type="hidden" name="url" value="'.$url.'"/><input id="nabice_tv" class="button" style="width:100%;height:50px;border: 0px none;background-color:#eee;" type="submit" value="Play On TV" onclick="this.style.display=\'none\';"/></form><iframe style="width:0;height:0;display:none" name="flvcd" id="flvcd"></iframe><script>setTimeout("document.getElementById(\'nabice_tv\').style.display=\'none\';", 30000);</script>';
 if(strpos($_SERVER["REQUEST_URI"], "v_show")){
 	$content = preg_replace("/ id=\"(q|m)header\"/", ' style="display:none;" id="$1header"', $content);
 	echo preg_replace("/(<body[^>]*>)/", '$1'.$replace_str, $content);
